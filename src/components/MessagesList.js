89f8bb78d83677d2b78d0bbe200a0036f4180235
apgsn <apgsn@yahoo.com>
2019-02-02 11:37:38 +0100

    Create basic interface with redux

---
+++ src/components/MessagesList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Message from "./Message.js";
+
+const MessagesList = ({ messages }) => (
+  <section id="messages-list">
+    <ul>
+      {messages.map(msg => (
+        <Message key={msg.id} {...msg} />
+      ))}
+    </ul>
+  </section>
+);
+
+MessagesList.PropTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+};
+
+export default MessagesList;
---
+++ src/reducers/users.js
@@ -0,0 +1,19 @@
+import * as types from "../actions/types.js";
+
+const users = (state = [], action) => {
+  switch (action.type) {
+    case types.ADD_USER:
+      return state.concat([
+        {
+          name: action.name,
+          id: action.id
+        }
+      ]);
+    case types.USERS_LIST:
+      return action.users;
+    default:
+      return state;
+  }
+};
+
+export default users;
